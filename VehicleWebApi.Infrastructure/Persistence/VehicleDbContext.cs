103bfed780e3b3a0331318d771dfd5cfb2339f05
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-03-04 14:47:02 -0300

    Created and configured the VehicleDbContext class in the Infrastructure layer

---
+++ VehicleWebApi.Infrastructure/Persistence/VehicleDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleWebApi.Domain.Entities;
+
+namespace VehicleWebApi.Infrastructure.Persistence
+{
+    public class VehicleDbContext : DbContext
+    {
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public DbSet<Model> Models { get; set; }
+
+        public DbSet<ModelVersion> ModelVersions { get; set; }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Truck> Trucks { get; set; }
+
+        public DbSet<Motorcycle> Motorcycles { get; set; }
+
+        public DbSet<BodyType> BodyTypes { get; set; }
+
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
+        public VehicleDbContext(DbContextOptions<VehicleDbContext> options) 
+            : base(options){ }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Aplicar todas as configurações Fluent API automaticamente
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(VehicleDbContext).Assembly);
+        }
+
+
+    }
+}
