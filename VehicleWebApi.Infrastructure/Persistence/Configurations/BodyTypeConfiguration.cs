4362d7d78035d6c37109a2c197d99aa435b6b567
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-03-04 18:17:25 -0300

    Configuration classes implemented - Fluent API Configuration (VehicleTypeConfiguration and BodyTypeConfiguration )

---
+++ VehicleWebApi.Infrastructure/Persistence/Configurations/BodyTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VehicleWebApi.Domain.Entities;
+
+namespace VehicleWebApi.Infrastructure.Persistence.Configurations
+{
+    public class BodyTypeConfiguration : IEntityTypeConfiguration<BodyType>
+    {
+        public void Configure(EntityTypeBuilder<BodyType> builder)
+        {
+            builder.ToTable("BodyType");
+
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(80);
+
+            builder.HasIndex(b => b.Name)
+                .IsUnique();
+        }
+    }
+}
---
+++ VehicleWebApi.Infrastructure/Persistence/Configurations/VehicleTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VehicleWebApi.Domain.Entities;
+
+namespace VehicleWebApi.Infrastructure.Persistence.Configurations
+{
+    public class VehicleTypeConfiguration : IEntityTypeConfiguration<VehicleType>
+    {
+        public void Configure(EntityTypeBuilder<VehicleType> builder)
+        {
+            builder.ToTable("VehicleType");
+
+            builder.HasKey(v => v.Id);
+
+            builder.Property(v => v.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.HasIndex(v => v.Name)
+                .IsUnique();
+        }
+    }
+}
